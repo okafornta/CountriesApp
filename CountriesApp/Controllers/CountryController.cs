2d9b8d2b0b80c5f430e82a33212b8fc140b56d2b
Okafornta <hillaryonia@gmail.com>
2022-11-29 16:18:31 +0100

    Added few things

--- CountriesApp/Controllers/CountryController.cs
+++ CountriesApp/Controllers/CountryController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CountriesApp.Data;
+using CountriesApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CountriesApp.Controllers
 {
     public class CountryController : Controller
     {
-        public IActionResult Index()
+        private readonly CountryDbContext _db;
+        public CountryController(CountryDbContext db)
+        {
+            _db = db;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Countries.ToListAsync());
+        }
+
+
+        public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Country country)
+        {
+            _db.Add(country);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
--- CountriesApp/Models/Country.cs
+++ CountriesApp/Models/Country.cs
@@ -10,6 +10,6 @@ namespace CountriesApp.Models
         public string? Capital { get; set; }
         public int  Population  { get; set; }
         public decimal Economy { get; set; }
-        public string? Curreny { get; set; }
+        public string? Currency { get; set; }
     }
 }
