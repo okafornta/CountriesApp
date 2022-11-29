f59b77c2684b235e811431f53a681fa191d50954
Okafornta <hillaryonia@gmail.com>
2022-11-29 16:19:33 +0100

    Added Details

--- CountriesApp/Controllers/CountryController.cs
+++ CountriesApp/Controllers/CountryController.cs
@@ -33,5 +33,38 @@ namespace CountriesApp.Controllers
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        // Get: countries details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var country = await _db.Countries
+                .FirstOrDefaultAsync(m => m.Id ==id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+            return View(country);
+        }
+        
+        // Get: Creating Editing side
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _db.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
     }
 }
