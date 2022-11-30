825c65466cb55756e267110b7405e3f42e70759c
Okafornta <hillaryonia@gmail.com>
2022-11-30 17:54:26 +0100

    Added Delete and Edit function

--- CountriesApp/Controllers/CountryController.cs
+++ CountriesApp/Controllers/CountryController.cs
@@ -66,5 +66,76 @@ namespace CountriesApp.Controllers
             return View();
         }
 
+        // Creating another edit and protecting from over posting 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Capital,Population,Economy,Currency")] Country country)
+        {
+            if (id != country.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(country);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CountryExists(country.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(country);
+        }
+
+        private bool CountryExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Creating Countries/Delete/
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _db.Countries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+        // POST: Countries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var countries = await _db.Countries.FindAsync(id);
+            _db.Countries.Remove(countries);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CountryExists(int? id)
+        {
+            return _db.Countries.Any(e => e.Id == id);
+        }
+
     }
 }
